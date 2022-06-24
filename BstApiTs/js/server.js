34c67b75ca1ec4b2404acfb5d9120b8019c860ef
Sna25 <mattsna@gmail.com>
2022-06-24 13:05:31 +0200

    [BstTree] Remove js console, remove ts server, add js server

--- BstApiTs/js/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http = require("http");
-const port = process.env.port || 1425;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bst Api\n');
-}).listen(port);
-//# sourceMappingURL=server.js.map
--- BstApiTs/server.ts
+++
@@ -1,6 +0,0 @@
-import http = require('http');
-const port = process.env.port || 1425
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Bst Api\n');
-}).listen(port);
